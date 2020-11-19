418852b2fa080dba022118dbead57d093786d1c0
HuynhChiToan <68183049+HuynhChiToan@users.noreply.github.com>
2020-11-19 10:24:11 +0700

    Update ServiceHistory.cs

--- ServiceHistory.cs
+++ ServiceHistory.cs
@@ -10,6 +10,23 @@ namespace ObjectOrientedProject
 	{
 		public ServiceHistory()
 		{
+		public LinkedList<Record> listOfServiceEngine;
+		public LinkedList<Record> listOfServiceTransmission;
+		public LinkedList<Record> listOfServiceTire;
+		public ServiceHistory(){
+			listOfServiceEngine = new LinkedList<Record>();
+			listOfServiceTransmission = new LinkedList<Record>();
+			listOfServiceTire = new LinkedList<Record>();
+		}
+		public void addlistOfServiceEngine(Record newRecord) {
+			listOfServiceEngine.AddLast(newRecord);
+		}
+		public void addlistOfServiceTransmission(Record newRecord) {
+			listOfServiceTransmission.AddLast(newRecord);
+		}
+		public void addlistOfServiceTire(Record newRecord) {
+			listOfServiceTire.AddLast(newRecord);
+		}
 		}
 	}
 }
